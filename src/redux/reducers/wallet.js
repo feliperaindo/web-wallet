e1ab53f9108fbac6191c57317401232532d717cf
Felipe <felipe.raindo@gmail.com>
2023-08-25 17:01:25 -0300

    update

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -6,7 +6,7 @@ import {
   EDIT_EXPENSE,
   ENABLE_EDITOR,
   REMOVE_EXPENSE,
-  SAVE_ID_EDIT
+  SAVE_ID_EDIT,
 } from '../actions/actionsTypes';
 
 const INITIAL_STATE = {
@@ -19,15 +19,15 @@ const INITIAL_STATE = {
 
 const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ADD_EXPENSE: return { ...state, expenses: [...state.expenses, action.payload] };
-    case EDIT_EXPENSE: return { ...state, expenses: action.payload };
-    case SAVE_ID_EDIT: return { ...state, idToEdit: action.payload };
-    case ENABLE_EDITOR: return { ...state, editor: true };
-    case DISABLE_EDITOR: return { ...state, editor: false };
-    case REMOVE_EXPENSE: return { ...state, expenses: action.payload };
-    case ADD_CURRENCIES: return { ...state, currencies: [...action.payload] };
-    case ADD_NAME_CURRENCIES: return { ...state, nameCurrencies: { ...action.payload } };
-    default: return state;
+  case ADD_EXPENSE: return { ...state, expenses: [...state.expenses, action.payload] };
+  case EDIT_EXPENSE: return { ...state, expenses: action.payload };
+  case SAVE_ID_EDIT: return { ...state, idToEdit: action.payload };
+  case ENABLE_EDITOR: return { ...state, editor: true };
+  case DISABLE_EDITOR: return { ...state, editor: false };
+  case REMOVE_EXPENSE: return { ...state, expenses: action.payload };
+  case ADD_CURRENCIES: return { ...state, currencies: [...action.payload] };
+  case ADD_NAME_CURRENCIES: return { ...state, nameCurrencies: { ...action.payload } };
+  default: return state;
   }
 };
 
