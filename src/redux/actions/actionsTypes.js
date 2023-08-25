b69b7f4cf88c5101954d55bc7ef95311267d050d
Felipe <felipe.raindo@gmail.com>
2023-08-25 16:46:24 -0300

    update

--- src/redux/actions/actionsTypes.js
+++ src/redux/actions/actionsTypes.js
@@ -1,9 +1,9 @@
 export const LOGIN = 'login';
-export const ADD_CURRENCIES = 'add currencies';
 export const ADD_EXPENSE = 'add expense';
-export const ADD_NAME_CURRENCIES = 'add currencies names';
-export const REMOVE_EXPENSE = 'remove expense';
+export const SAVE_ID_EDIT = 'save id to edit item in array';
 export const EDIT_EXPENSE = 'edit expense';
 export const ENABLE_EDITOR = 'enable editor';
+export const REMOVE_EXPENSE = 'remove expense';
+export const ADD_CURRENCIES = 'add currencies';
 export const DISABLE_EDITOR = 'disable editor';
-export const SAVE_ID_EDIT = 'save id to edit item in array';
+export const ADD_NAME_CURRENCIES = 'add currencies names';
