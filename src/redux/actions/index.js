1c1b750423e674f8ffdf07677d7ebda2f6a7a2c5
Felipe <felipe.raindo@gmail.com>
2023-08-25 16:37:57 -0300

    update

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -7,8 +7,8 @@ export const login = (email) => ({ type: LOGIN, payload: email });
 export const addCurrencies = (currencies) => (
   { type: ADD_CURRENCIES, payload: currencies });
 
-export const addFullNameCurrencies = (currencies) => (
-  { type: ADD_NAME_CURRENCIES, payload: currencies });
+export const addFullNameCurrencies = (currencies) => ({
+  type: ADD_NAME_CURRENCIES, payload: currencies });
 
 export const addExpense = (newExpense) => ({ type: ADD_EXPENSE, payload: newExpense });
 
